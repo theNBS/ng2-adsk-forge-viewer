ad6b43299ad79ba0b548a3e640f27c3277079684
Alan Smith <alan.smith@ribaenterprises.com>
2018-02-07 11:45:00 +0000

    List events that can be bound to

--- src/app/modules/viewer/extensions/basic-extension.ts
+++ src/app/modules/viewer/extensions/basic-extension.ts
@@ -10,6 +10,48 @@ export class BasicExtension extends Autodesk.Viewing.Extension {
   private viewer: Autodesk.Viewing.Viewer3D;
   private extOptions: Autodesk.Viewing.ExtensionOptions;
 
+  private readonly events: string[] = [
+    Autodesk.Viewing.AGGREGATE_SELECTION_CHANGED_EVENT,
+    Autodesk.Viewing.ANIMATION_READY_EVENT,
+    Autodesk.Viewing.CAMERA_CHANGE_EVENT,
+    Autodesk.Viewing.CUTPLANES_CHANGE_EVENT,
+    Autodesk.Viewing.ESCAPE_EVENT,
+    Autodesk.Viewing.EXPLODE_CHANGE_EVENT,
+    Autodesk.Viewing.EXTENSION_LOADED_EVENT,
+    Autodesk.Viewing.EXTENSION_UNLOADED_EVENT,
+    Autodesk.Viewing.FINAL_FRAME_RENDERED_CHANGED_EVENT,
+    Autodesk.Viewing.FIT_TO_VIEW_EVENT,
+    Autodesk.Viewing.FRAGMENTS_LOADED_EVENT,
+    Autodesk.Viewing.FULLSCREEN_MODE_EVENT,
+    Autodesk.Viewing.GEOMETRY_LOADED_EVENT,
+    Autodesk.Viewing.HIDE_EVENT,
+    Autodesk.Viewing.HYPERLINK_EVENT,
+    Autodesk.Viewing.ISOLATE_EVENT,
+    Autodesk.Viewing.LAYER_VISIBILITY_CHANGED_EVENT,
+    Autodesk.Viewing.LOAD_MISSING_GEOMETRY,
+    Autodesk.Viewing.MODEL_ROOT_LOADED_EVENT,
+    Autodesk.Viewing.MODEL_UNLOADED_EVENT,
+    Autodesk.Viewing.NAVIGATION_MODE_CHANGED_EVENT,
+    Autodesk.Viewing.OBJECT_TREE_CREATED_EVENT,
+    Autodesk.Viewing.OBJECT_TREE_UNAVAILABLE_EVENT,
+    Autodesk.Viewing.PREF_CHANGED_EVENT,
+    Autodesk.Viewing.PREF_RESET_EVENT,
+    Autodesk.Viewing.PROGRESS_UPDATE_EVENT,
+    Autodesk.Viewing.RENDER_OPTION_CHANGED_EVENT,
+    Autodesk.Viewing.RENDER_PRESENTED_EVENT,
+    Autodesk.Viewing.RESET_EVENT,
+    Autodesk.Viewing.RESTORE_DEFAULT_SETTINGS_EVENT,
+    Autodesk.Viewing.SELECTION_CHANGED_EVENT,
+    Autodesk.Viewing.SHOW_EVENT,
+    Autodesk.Viewing.TEXTURES_LOADED_EVENT,
+    Autodesk.Viewing.TOOL_CHANGE_EVENT,
+    Autodesk.Viewing.VIEWER_INITIALIZED,
+    Autodesk.Viewing.VIEWER_RESIZE_EVENT,
+    Autodesk.Viewing.VIEWER_STATE_RESTORED_EVENT,
+    Autodesk.Viewing.VIEWER_UNINITIALIZED,
+    Autodesk.Viewing.SELECTION_CHANGED_EVENT,
+  ];
+
   static get extensionName() {
     return 'BasicExtension';
   }
@@ -18,8 +60,8 @@ export class BasicExtension extends Autodesk.Viewing.Extension {
     Autodesk.Viewing.theExtensionManager.registerExtension(BasicExtension.extensionName, BasicExtension);
   }
 
-  static onViewerSelectionEvent(args: Autodesk.Viewing.SelectionChangedEventArgs) {
-    console.log('item selected:', args.fragIdsArray, args.dbIdArray, args.nodeArray, args.model);
+  static onViewerEvent(args: Autodesk.Viewing.ViewerEventArgs) {
+    console.log('Event fired', args);
     BasicExtension.publishEvent(args.type, args);
   }
 
@@ -59,15 +101,19 @@ export class BasicExtension extends Autodesk.Viewing.Extension {
   }
 
   load() {
+    this.events.forEach((eventName) => {
+      this.viewer.addEventListener(eventName, BasicExtension.onViewerEvent.bind(this));
+    });
+
     console.log(BasicExtension.extensionName, 'loaded!');
-    this.viewer.addEventListener(Autodesk.Viewing.SELECTION_CHANGED_EVENT,
-                                 BasicExtension.onViewerSelectionEvent.bind(this));
     return true;
   }
 
   unload() {
-    this.viewer.removeEventListener(Autodesk.Viewing.SELECTION_CHANGED_EVENT,
-                                    BasicExtension.onViewerSelectionEvent.bind(this));
+    this.events.forEach((eventName) => {
+      this.viewer.removeEventListener(eventName, BasicExtension.onViewerEvent.bind(this));
+    });
+
     console.log(BasicExtension.extensionName, 'unloaded!');
     return true;
   }
