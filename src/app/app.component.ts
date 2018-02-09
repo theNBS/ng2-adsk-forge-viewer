a25d8d47366bf07e9f454bc5dbe5b481fe74f7a5
Alan Smith <alan.smith@ribaenterprises.com>
2018-02-09 21:57:50 +0000

    Add route outlet so we can test component load and unload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,28 +4,7 @@ import { ViewerOptions } from './modules/viewer/viewer.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'app';
-
-  public viewerOptions: ViewerOptions;
-  public documentId: string;
-
-  setViewerOptions() {
-    this.viewerOptions = {
-      initializerOptions: {
-        env: 'AutodeskProduction',
-        getAccessToken: (onGetAccessToken: (token: string, expire: number) => void) => {
-          const accessToken = '<YOUR_APPLICATION_TOKEN>';
-          const expireTimeSeconds = 60 * 30;
-          onGetAccessToken(accessToken, expireTimeSeconds);
-        },
-      },
-    };
-  }
-
-  loadDocument() {
-    this.documentId = '<YOUR_URN_ID>';
-  }
 }
