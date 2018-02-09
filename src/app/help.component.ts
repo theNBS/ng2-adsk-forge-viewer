ef1cca22218aba62f9583ed5e02ceaa60586513a
Alan Smith <alan.smith@ribaenterprises.com>
2018-02-09 23:44:16 +0000

    Add missing files

---
+++ src/app/help.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-help',
+  templateUrl: './help.component.html',
+})
+export class HelpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/viewer-container.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { DocumentChangedEvent, ViewerOptions } from './modules/viewer/viewer.component';
+
+@Component({
+  selector: 'app-viewer-container',
+  templateUrl: './viewer-container.component.html',
+  styleUrls: ['./viewer-container.component.scss'],
+})
+export class ViewerContainerComponent {
+
+  public viewerOptions: ViewerOptions;
+  public documentId: string;
+
+  setViewerOptions() {
+    this.viewerOptions = {
+      initializerOptions: {
+        env: 'AutodeskProduction',
+        getAccessToken: (onGetAccessToken: (token: string, expire: number) => void) => {
+          const accessToken = '<TOKEN_GOES_HERE>';
+          const expireTimeSeconds = 60 * 30;
+          onGetAccessToken(accessToken, expireTimeSeconds);
+        },
+      },
+      // showFirstViewable: false,
+      // headlessViewer: true,
+    };
+  }
+
+  loadDocument() {
+    this.documentId = '<DOCUMENT_URN_GOES_HERE>';
+  }
+
+  documentChanged(event: DocumentChangedEvent) {
+    // const viewerApp = event.viewingApplication;
+    // const viewables = viewerApp.bubble.search({ type: 'geometry' });
+    //
+    // if (viewables && viewables.length > 0) {
+    //   event.viewerComponent.selectItem(viewables[0].data);
+    //   // viewerApp.selectItem(viewables[0].data, undefined, undefined);
+    // }
+  }
+}
