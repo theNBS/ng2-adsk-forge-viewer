3c3f16f0bd326c381fad979e41cea8fcf9e20e2f
Alan Smith <alan.smith@ribaenterprises.com>
2018-03-30 19:24:12 +0100

    Configure demo app

--- src/demo/app/app.component.ts
+++ src/demo/app/app.component.ts
@@ -1,9 +1,43 @@
 import { Component } from '@angular/core';
+import { ViewerOptions, ViewingApplicationInitializedEvent, DocumentChangedEvent } from 'ng2-adsk-forge-viewer';
+
+const ACCESS_TOKEN = '<TOKEN_GOES_HERE>';
+const DOCUMENT_URN = '<DOCUMENT_URN_GOES_HERE>';
 
 @Component({
   selector: 'demo-app',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor() { }
+  public viewerOptions: ViewerOptions;
+  public documentId: string;
+
+  setViewerOptions() {
+    this.viewerOptions = {
+      initializerOptions: {
+        env: 'AutodeskProduction',
+        getAccessToken: (onGetAccessToken: (token: string, expire: number) => void) => {
+          const expireTimeSeconds = 60 * 30;
+          onGetAccessToken(ACCESS_TOKEN, expireTimeSeconds);
+        },
+      },
+      // showFirstViewable: false,
+      // headlessViewer: true,
+    };
+  }
+
+  loadDocument(event: ViewingApplicationInitializedEvent) {
+    event.viewerComponent.DocumentId = DOCUMENT_URN;
+  }
+
+  documentChanged(event: DocumentChangedEvent) {
+    // const viewerApp = event.viewingApplication;
+    // const viewables = viewerApp.bubble.search({ type: 'geometry' });
+    //
+    // if (viewables && viewables.length > 0) {
+    //   event.viewerComponent.selectItem(viewables[0].data);
+    //   // viewerApp.selectItem(viewables[0].data, undefined, undefined);
+    // }
+  }
 }
