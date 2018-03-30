f925499ebff75b786cc659fd1a724a4ba759517f
Alan Smith <alan.smith@ribaenterprises.com>
2018-03-30 19:41:47 +0100

    Configure demo app

--- src/demo/app/app.component.js
+++ src/demo/app/app.component.js
@@ -5,20 +5,45 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var ACCESS_TOKEN = 'eyJhbGciOiJIUzI1NiIsImtpZCI6Imp3dF9zeW1tZXRyaWNfa2V5In0.eyJjbGllbnRfaWQiOiJuVFh0TGhXdUg5YzVtNmpValVEZTF6SmxGcGpSaE96YyIsImV4cCI6MTUyMjQzNzg5MCwic2NvcGUiOlsiYnVja2V0OmNyZWF0ZSIsImJ1Y2tldDpyZWFkIiwiZGF0YTp3cml0ZSIsImRhdGE6cmVhZCIsImJ1Y2tldDpkZWxldGUiXSwiYXVkIjoiaHR0cHM6Ly9hdXRvZGVzay5jb20vYXVkL2p3dGV4cDYwIiwianRpIjoiOFJFb05qZ2JlNXNkTDJWcFA1a0hBeVhhSEQxNGJWTmFHSHdKN0lMYkI4NEM3a2Y4ZWJCMkJCWnF2Sk1JU1NIQSJ9.2C1fbGA6ydid4AO4g1j8UoZVzQKemhP-bnjhbPYYHms';
+var DOCUMENT_URN = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dG9vbGtpdC9BMTY4MThBMDMxNTYyM0M5MEY2QUVGQkNENjdDRDRFQi5ydnQ';
 var AppComponent = (function () {
     function AppComponent() {
     }
+    AppComponent.prototype.setViewerOptions = function () {
+        this.viewerOptions = {
+            initializerOptions: {
+                env: 'AutodeskProduction',
+                getAccessToken: function (onGetAccessToken) {
+                    var expireTimeSeconds = 60 * 30;
+                    onGetAccessToken(ACCESS_TOKEN, expireTimeSeconds);
+                },
+            },
+        };
+    };
+    AppComponent.prototype.loadDocument = function (event) {
+        event.viewerComponent.DocumentId = DOCUMENT_URN;
+    };
+    AppComponent.prototype.documentChanged = function (event) {
+        // const viewerApp = event.viewingApplication;
+        // const viewables = viewerApp.bubble.search({ type: 'geometry' });
+        //
+        // if (viewables && viewables.length > 0) {
+        //   event.viewerComponent.selectItem(viewables[0].data);
+        //   // viewerApp.selectItem(viewables[0].data, undefined, undefined);
+        // }
+    };
+    AppComponent.prototype.selectionChanged = function (event) {
+        console.log(event);
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'demo-app',
             templateUrl: './app.component.html',
-        }),
-        __metadata("design:paramtypes", [])
+            styleUrls: ['./app.component.css'],
+        })
     ], AppComponent);
     return AppComponent;
 }());
--- src/demo/app/app.component.ts
+++ src/demo/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { ViewerOptions, ViewingApplicationInitializedEvent, DocumentChangedEvent } from 'ng2-adsk-forge-viewer';
 
-const ACCESS_TOKEN = '<TOKEN_GOES_HERE>';
-const DOCUMENT_URN = '<DOCUMENT_URN_GOES_HERE>';
+const ACCESS_TOKEN = 'eyJhbGciOiJIUzI1NiIsImtpZCI6Imp3dF9zeW1tZXRyaWNfa2V5In0.eyJjbGllbnRfaWQiOiJuVFh0TGhXdUg5YzVtNmpValVEZTF6SmxGcGpSaE96YyIsImV4cCI6MTUyMjQzNzg5MCwic2NvcGUiOlsiYnVja2V0OmNyZWF0ZSIsImJ1Y2tldDpyZWFkIiwiZGF0YTp3cml0ZSIsImRhdGE6cmVhZCIsImJ1Y2tldDpkZWxldGUiXSwiYXVkIjoiaHR0cHM6Ly9hdXRvZGVzay5jb20vYXVkL2p3dGV4cDYwIiwianRpIjoiOFJFb05qZ2JlNXNkTDJWcFA1a0hBeVhhSEQxNGJWTmFHSHdKN0lMYkI4NEM3a2Y4ZWJCMkJCWnF2Sk1JU1NIQSJ9.2C1fbGA6ydid4AO4g1j8UoZVzQKemhP-bnjhbPYYHms';
+const DOCUMENT_URN = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dG9vbGtpdC9BMTY4MThBMDMxNTYyM0M5MEY2QUVGQkNENjdDRDRFQi5ydnQ';
 
 @Component({
   selector: 'demo-app',
@@ -39,5 +39,9 @@ export class AppComponent {
     //   event.viewerComponent.selectItem(viewables[0].data);
     //   // viewerApp.selectItem(viewables[0].data, undefined, undefined);
     // }
+  }
+
+  selectionChanged(event: Autodesk.Viewing.SelectionChangedEventArgs) {
+    console.log(event);
   }
 }
