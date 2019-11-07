13eb21a18d6507c71d4570bd36c7d470a926c1dd
Alan Smith <alan.smith@thenbs.com>
2019-11-07 21:48:02 +0000

    Fix lint error

--- projects/ng2-adsk-forge-viewer/src/lib/component/viewer.component.ts
+++ projects/ng2-adsk-forge-viewer/src/lib/component/viewer.component.ts
@@ -371,7 +371,7 @@ export class ViewerComponent implements OnDestroy {
       return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
     };
 
-    const guid = (S4() + S4() + '-' + S4() + '-' +S4() + '-' + S4() + '-' + S4() + S4() + S4());
+    const guid = (S4() + S4() + '-' + S4() + '-' + S4() + '-' + S4() + '-' + S4() + S4() + S4());
     return `viewer_${guid}`;
   }
 }
